fd53855ddec26d2850c00bdbd01ed2134a39c402
Erika <Erika1111@yandex.ru>
2023-01-03 00:13:39 +0300

    homework6

---
+++ Atask1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.WriteLine("Величина массива ");
+int input = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[input];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+  Console.WriteLine($"Введите элемент массива под индексом {i}:\t");  
+  array[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count++; 
+    
+}
+Console.Write($"колличество элементов > 0 {count}");
